d8d7735979df5c9115746986fc59bb06e83a4614
daviromao <davi.romao0210@gmail.com>
2022-06-22 19:50:19 -0300

    feat: create post model and register in admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        ('withdrawn', 'Withdrawn'),
+    ]
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    summary = models.TextField()
+    body = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    published = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
+    
