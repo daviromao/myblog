91f2fdaaa594d4aee75de4bcba857733a266b72a
daviromao <davi.romao0210@gmail.com>
2022-06-24 21:56:46 -0300

    feat: added post url in title

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from blog.views import PostDetailView, PostListView
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', PostListView.as_view()),
-    path('<slug:slug>/', PostDetailView.as_view()),
+    path('', PostListView.as_view(), name='post_list'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
