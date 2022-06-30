c6849df0881266faa1379294bccf836de3eea782
daviromao <davi.romao0210@gmail.com>
2022-06-30 11:27:12 -0300

    refactor(blog): create base model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
-class Post(models.Model):
+class Base(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    abstract = True
+
+
+class Post(Base):
     STATUS_CHOICES = [
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -14,8 +22,6 @@ class Post(models.Model):
     body = models.TextField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     published = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-published',)
